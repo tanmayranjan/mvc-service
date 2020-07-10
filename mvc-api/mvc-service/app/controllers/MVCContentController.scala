1df82153f3938e3909ba5de3f96c747f8c6a2352
Tanmay Ranjan <tanmay.ranjan9@gmail.com>
2020-07-10 18:03:54 +0530

    removed csv support

--- mvc-api/mvc-service/app/controllers/MVCContentController.scala
+++ mvc-api/mvc-service/app/controllers/MVCContentController.scala
@@ -12,27 +12,8 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class MVCContentController @Inject()(cc: ControllerComponents) (implicit exec: ExecutionContext) extends SearchBaseController(cc) {
   val readJson: ReadJson = new ReadJson()
-  val processCSV: CSVContentProcessor = new CSVContentProcessor()
-  val readCSV: ReadCSV = new ReadCSV();
   @transient val mapper = new ObjectMapper();
   mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-  def createUsingCSV() = Action.async { implicit request =>
-    val body = request.body.asJson.getOrElse("{}").toString
-    var result = ResponseHandler.OK()
-    if(body.equals("{}")) {
-      val multipart = request.body.asMultipartFormData
-      val file = multipart.get.file("File").get.ref;
-      Future {
-        val rows = readCSV.readCSV(file)
-        processCSV.processCSVRows(rows)
-      }
-    }
-    else {
-      result = ResponseHandler.ERROR(ResponseCode.CLIENT_ERROR,"400","Bad Request,Please provide valid input")
-    }
-    val response = mapper.writeValueAsString(result);
-    Future(Ok(response).as("application/json"))
-  }
   def createUsingJson() = Action.async { implicit request =>
     val body = request.body.asJson.getOrElse("{}").toString
     var result = ResponseHandler.OK()

93a3a13b70d509ca5f6c0b630fb2bdea710500f7
Tanmay Ranjan <tanmay.ranjan9@gmail.com>
2020-07-31 15:20:07 +0530

    trial for test cases

--- mvc-api/mvc-service/test/managers/ReadJsonTest.java
+++ mvc-api/mvc-service/test/managers/ReadJsonTest.java
@@ -1,5 +1,6 @@
 package managers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,21 +10,36 @@ import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.sunbird.kafka.client.KafkaClient;
+
+import java.io.IOException;
+import java.util.Map;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({GetContentDefinition.class})
+@PrepareForTest({GetContentDefinition.class,Postman.class, KafkaClient.class})
 public class ReadJsonTest {
+    ObjectMapper mapper = new ObjectMapper();
 private String req = "{\"request\":{\"content\":[{\"board\":\"State(Tamil Nadu)\",\"subject\":[\"English\"],\"medium\":[\"Telegu\"],\"gradeLevel\":[\"Class 4\",\"Class 5\",\"Class 6\"],\"textbook_name\":[\"Science\"],\"level1Name\":[\"Sorting Materials Into Groups\"],\"level1Concept\":[\"Materials\"],\"source\":[\"Diksha 1\"],\"sourceURL\":[\"https://diksha.gov.in/play/content/do_31283180221267968012331\"]}]}}";
-    @Before
+private String getResponse = "{\"id\":\"api.content.read\",\"ver\":\"1.0\",\"ts\":\"2020-07-31T09:29:44.690Z\",\"params\":{\"resmsgid\":\"5e847d20-d310-11ea-bb67-2b84cd2a1815\",\"msgid\":\"5e83e0e0-d310-11ea-bb67-2b84cd2a1815\",\"status\":\"successful\",\"err\":null,\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"content\":{\"ownershipType\":[\"createdBy\"],\"copyright\":\"Ekstep\",\"previewUrl\":\"https://ekstep-public-prod.s3-ap-south-1.amazonaws.com/content/ecml/do_31249815833451724828554-latest\",\"keywords\":[\"Story\"],\"subject\":[\"Mathematics\"],\"channel\":\"in.ekstep\",\"downloadUrl\":\"https://ekstep-public-prod.s3-ap-south-1.amazonaws.com/ecar_files/do_31249815833451724828554/squares-and-square-roots_1559121118657_do_31249815833451724828554_5.0.ecar\",\"language\":[\"English\"],\"source\":\"\",\"mimeType\":\"application/vnd.ekstep.ecml-archive\",\"variants\":{\"spine\":{\"ecarUrl\":\"https://ekstep-public-prod.s3-ap-south-1.amazonaws.com/ecar_files/do_31249815833451724828554/squares-and-square-roots_1559121118938_do_31249815833451724828554_5.0_spine.ecar\",\"size\":28269}},\"objectType\":\"Content\",\"gradeLevel\":[\"Class 8\"],\"me_totalRatingsCount\":3,\"appIcon\":\"https://ekstep-public-prod.s3-ap-south-1.amazonaws.com/content/do_31249815833451724828554/artifact/assetsmath_square_1_6112_1521832091_1521832091202.thumb.png\",\"appId\":\"prod.diksha.app\",\"copyType\":\"Enhance\",\"contentEncoding\":\"gzip\",\"artifactUrl\":\"https://ekstep-public-prod.s3-ap-south-1.amazonaws.com/content/do_31249815833451724828554/artifact/1527141453728_do_31249815833451724828554.zip\",\"collaborators\":[\"5469\"],\"me_totalPlaySessionCount\":\"{\\\"portal\\\":15}\",\"sYS_INTERNAL_LAST_UPDATED_ON\":\"2020-07-31T03:05:38.679+0000\",\"contentType\":\"Resource\",\"identifier\":\"do_31249815833451724828554\",\"lastUpdatedBy\":\"539\",\"audience\":[\"Learner\"],\"me_totalTimeSpentInSec\":\"{\\\"portal\\\":1252}\",\"visibility\":\"Default\",\"author\":\"Ekstep\",\"consumerId\":\"89490534-126f-4f0b-82ac-3ff3e49f3468\",\"mediaType\":\"content\",\"ageGroup\":[\">10\"],\"osId\":\"org.ekstep.quiz.app\",\"languageCode\":[\"en\"],\"lastPublishedBy\":\"ekstep\",\"version\":2,\"license\":\"CC BY 4.0\",\"prevState\":\"Review\",\"size\":516641,\"lastPublishedOn\":\"2019-05-29T09:11:58.657+0000\",\"domain\":[\"numeracy\"],\"name\":\"Squares and square roots\",\"publisher\":\"\",\"attributions\":[\"\"],\"status\":\"Live\",\"totalQuestions\":14,\"code\":\"org.ekstep.literacy.story.17889.fork.6112.1525654093\",\"origin\":\"do_312474333408550912120050\",\"description\":\"Students Resources\",\"streamingUrl\":\"https://ekstep-public-prod.s3-ap-south-1.amazonaws.com/content/ecml/do_31249815833451724828554-latest\",\"posterImage\":\"https://ekstep-public-prod.s3-ap-south-1.amazonaws.com/content/do_312466848489717760216939/artifact/assetsmath_square_1_6112_1521832091_1521832091202.png\",\"idealScreenSize\":\"normal\",\"createdOn\":\"2018-05-24T00:56:26.363+0000\",\"copyrightYear\":2019,\"contentDisposition\":\"inline\",\"lastUpdatedOn\":\"2019-05-29T09:11:58.048+0000\",\"dialcodeRequired\":\"No\",\"owner\":\"Ruchi Mazumdar\",\"lastStatusChangedOn\":\"2019-05-30T11:35:14.409+0000\",\"creator\":\"Siri Duvvuru\",\"os\":[\"All\"],\"totalScore\":14,\"pkgVersion\":5,\"versionKey\":\"1539186559643\",\"idealScreenDensity\":\"hdpi\",\"framework\":\"NCF\",\"s3Key\":\"ecar_files/do_31249815833451724828554/squares-and-square-roots_1559121118657_do_31249815833451724828554_5.0.ecar\",\"me_averageRating\":4,\"lastSubmittedOn\":\"2019-05-29T09:02:03.562+0000\",\"createdBy\":\"6112\",\"compatibilityLevel\":2,\"board\":\"CBSE\",\"resourceType\":\"Read\"}}}";
+@Before
     public void setup(){
         MockitoAnnotations.initMocks(this);
     }
 
     @Test
-  public void testReadJson()  {
+  public void testReadJson() throws Exception {
         PowerMockito.mockStatic(GetContentDefinition.class);
+        PowerMockito.mock(KafkaClient.class);
+        PowerMockito.mockStatic(Postman.class);
         Mockito.when(GetContentDefinition.validateSourceURL(Mockito.anyString())).thenReturn(true);
+        Mockito.when(Postman.GET(Mockito.anyString())).thenReturn(getEvent(getResponse));
+        PowerMockito.doNothing().when(KafkaClient.class);
+        KafkaClient kafkaClient = new KafkaClient();
+        kafkaClient.send(Mockito.anyString(),Mockito.anyString());
         ReadJson readJson = new ReadJson();
         readJson.read(req);
+    }
+    public Map<String, Object> getEvent(String message) throws IOException {
+        return  mapper.readValue(message,Map.class);
     }
 }
